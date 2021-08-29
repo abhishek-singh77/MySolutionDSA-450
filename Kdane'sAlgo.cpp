f9776729826ec0bbc1383f894dd59ec1ed0b6ac6
Abhishek Singh <58261248+abhishek-singh77@users.noreply.github.com>
2021-08-29 22:12:53 +0530

    added

---
+++ Kdane'sAlgo.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+
+
+// Function to find subarray with maximum sum
+// arr: input array
+// n: size of array
+int maxSubarraySum(int arr[], int n){
+    
+    // Your code here
+    int sum=arr[0];
+    int mxend=arr[0];
+    for(int i=1; i<n; i++){
+        mxend=max(mxend+arr[i],arr[i]);
+        sum=max(sum,mxend);
+    }
+    return sum;
+}
+
+// { Driver Code Starts.
+
+int main()
+{
+    int t,n;
+    
+    cin>>t; //input testcases
+    while(t--) //while testcases exist
+    {
+        
+        cin>>n; //input size of array
+        
+        int a[n];
+        
+        for(int i=0;i<n;i++)
+            cin>>a[i]; //inputting elements of array
+        
+        cout << maxSubarraySum(a, n) << endl;
+    }
+}
+  // } Driver Code Ends
