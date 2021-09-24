64e0e73e3928f2a902f28be67ab58a9687bfad66
Abhishek Singh <58261248+abhishek-singh77@users.noreply.github.com>
2021-09-24 22:13:01 +0530

    added file

---
+++ cppDSA/Cylically rotate an array by one.cpp
@@ -0,0 +1,24 @@
+# include <iostream>
+using namespace std;
+
+int main() {
+    //code
+    
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        int arr[n+1];
+        for(int i=1;i<=n;i++){
+            cin>>arr[i];
+        }
+        arr[0]=arr[n];
+        for(int i=0;i<n;i++){
+            cout<<arr[i]<<" ";
+        }
+    cout<<'\n';
+    }
+    
+    return 0;
+}
